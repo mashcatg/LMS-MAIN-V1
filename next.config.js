575b200d72c8fdb6c9a72a5c3f6d078f3a096cdf
mashcatg <mashcatg@gmail.com>
2024-11-21 19:25:22 +0600

    1

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const config = {
   webpack: (config, { isServer }) => {
     // CKEditor fix
     config.resolve.alias = {
       ...config.resolve.alias,
       '@ckeditor/ckeditor5-build-classic': '@ckeditor/ckeditor5-build-classic/build/ckeditor',
     };
-
     return config;
   },
   // Add this to handle the createElement error
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+export default config;
