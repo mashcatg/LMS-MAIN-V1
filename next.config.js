6264414792483eb724d99de7a092399a1444292e
mashcatg <mashcatg@gmail.com>
2024-11-24 16:53:01 +0600

    789

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   basePath: '/app2',
+  output: 'standalone',
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;
